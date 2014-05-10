1a57a965db750d278d073bf6e0c7bd051517d003
hemanth.hm <hemanth.hm@gmail.com>
2014-05-10 15:49:03 +0530

    Random port selection logic

--- server.js
+++ server.js
@@ -25,7 +25,21 @@ var BheemServer = function() {
     self.setupVariables = function() {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 1338;
+        var IPv4 = (function(){
+          var interfaces = require('os').networkInterfaces(),
+          IP;
+          for (var iface in interfaces) {
+              interfaces[iface].forEach(function(addr) {
+                  if (addr.family == 'IPv4') {
+                      IP = addr.address;
+                  }
+             });
+          }
+          return IP;
+        }());
+
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || IPv4;
+
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
