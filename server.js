28be203d7b9e0dc9959c5e899f7d33a0b5d41864
Ashvini Kumar Saxena <ashvsaxe@cisco.com>
2014-05-10 19:43:35 +0530

    Blah!

--- server.js
+++ server.js
@@ -1,185 +1,45 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express   = require('express');
-var fs        = require('fs');
-var httpProxy = require('http-proxy');
-//var connection = require('./services/mysql/mysql');
 var WebSocketServer = require('ws').Server;
+var http = require('http');
 
-/**
- *  Define the sample application.
- */
-var BheemServer = function() {
-
-    //  Scope.
-    var self = this;
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000
 
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+var server = http.createServer();
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port = process.env.OPENSHIFT_NODEJS_PORT;
-        
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+var  wss = new WebSocketServer({server:server})
+console.log(wss);
+wss.on('connection', function(ws) {
+    console.log('/connection connected');
+    ws.on('message', function(data, flags) {
+        if (flags.binary) { return; }
+        console.log('>>> ' + data);
+        if (data == 'test') { console.log('test'); ws.send('got test'); }
+        if (data == 'hello') { console.log('hello'); ws.send('WAZZZUP!'); }
+    });
+    ws.on('close', function() {
+      console.log('Connection closed!');
+    });
+    ws.on('error', function(e) {
+      console.log(e);
+    });
+});
 
+wss.on('connect', function(ws) {
+    console.log('/connect connected');
+    ws.on('message', function(data, flags) {
+        if (flags.binary) { return; }
+        console.log('>>> ' + data);
+        if (data == 'test') { console.log('test'); ws.send('got test'); }
+        if (data == 'hello') { console.log('hello'); ws.send('WAZZZUP!'); }
+    });
+    ws.on('close', function() {
+      console.log('Connection closed!');
+    });
+    ws.on('error', function(e) {
+      console.log(e);
+    });
+});
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        /*
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-        */
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            /*var query = connection.query('INSERT INTO USER SET ?', req.query, function(err, result) {
-              // Neat!
-            });*/
-            res.send(self.cache_get('index.html') );
-        };
-
-        self.routes['/api*'] = function(req, res) {
-            req.url = req.url.substring(4);
-            req.url += '?api_key=9673fbfd2537cc9da1ac0d935ed23711';
-
-            console.log(req.url);
-            self.proxy.web(req, res);
-        }
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-        self.proxy = httpProxy.createProxyServer({target: 'http://api.themoviedb.org:80'});
-     
-          self.wss = new WebSocketServer({port:8000});
-
-          self.wss.on('connection', function(ws) {
-            ws.on('message', function(message) {
-                console.log('received: %s', message);
-                ws.send('Received: ' + message);
-            });
-            ws.send('something');
-          });
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var app = new BheemServer();
-app.initialize();
-app.start();
-
+console.log('Listening at IP ' + ipaddr +' on port '+port);
+server.listen(port,ipaddr);
