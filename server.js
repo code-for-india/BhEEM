0b4d27fe6e79237ab33d40309eaf666eee78001d
Ashvini Kumar Saxena <ashvsaxe@cisco.com>
2014-05-10 14:29:21 +0530

    Changed websocket

--- server.js
+++ server.js
@@ -137,6 +137,7 @@ var BheemServer = function() {
         self.wss.on('connection', function(ws) {
             ws.on('message', function(message) {
                 console.log('received: %s', message);
+                ws.send('Received: ' + message);
             });
             ws.send('something');
         });
